1dd71b7553090cf52e5189829decbbb180dfb8e2
Grisvian Gema <grisviangema@gmail.com>
2026-01-20 18:36:33 +0700

    menghubungkan login/daftar client dengan server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,21 @@ import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Login from './pages/Login'
+// 1. IMPORT INI
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+        {/* 2. PASANG INI DI SINI */}
+        <ToastContainer />
+        
         <Navbar />
         
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
-            {/* Route lain akan kita tambahkan nanti */}
         </Routes>
     </div>
   )
