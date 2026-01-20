0efa99471b4a9cc23c6668e48061f9ee5cee15fb
Grisvian Gema <grisviangema@gmail.com>
2026-01-20 10:27:30 +0700

    Membuat Fitur LOGIN Admin

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addPetugas } from '../controllers/adminController.js';
+import { addPetugas, loginAdmin } from '../controllers/adminController.js';
 import upload from '../middlewares/multer.js';
 
 const adminRouter = express.Router();
 
 // Endpoint: POST /api/admin/add-petugas
 adminRouter.post('/add-petugas', upload.single('image'), addPetugas);
+adminRouter.post('/login', loginAdmin); // <--- Tambahkan baris ini
 
 export default adminRouter;
