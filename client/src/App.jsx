b8ae86fef3dc7d762679921aaef4e8e22c496332
grisviangema <grisviangema@gmail.com>
2026-02-26 10:21:15 +0700

    Update App component styling and structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,35 +27,40 @@ import PajakMBLB from './pages/layanan/Mblb'
 
 const App = () => {
   return (
-    <div className='flex flex-col min-h-screen'>
+    <div className='flex flex-col min-h-screen bg-gray-50'>
       {/* Komponen Global */}
-      <ToastContainer position="top-right" autoClose={3000} />0
+      <ToastContainer position="top-right" autoClose={3000} />
       <AnnouncementPopup />
       
-      {/* 1. Navbar tetap di atas */}
-      <Navbar className="bg-white shadow-md"/>
+      {/* 1. Navbar: Gunakan w-full agar background memenuhi layar, 
+          tapi isi navbar di dalamnya harus dibatasi max-width (di dalam komponen Navbar) */}
+      <Navbar className="fixed top-0 left-0 w-full z-50 bg-white shadow-md"/>
 
       {/* 2. Main Content Wrapper */}
-      {/* pt-20 atau pt-24 sangat penting agar konten tidak tertutup Navbar Fixed */}
-      <main className='flex-grow pt-24 md:pt-14 bg-gray-50'>
+      <main className='flex-grow pt-24 md:pt-28'>
         
-        {/* Konten dengan pembatas lebar (Container) */}
-        <div className='mx-4 sm:mx-[5%] max-w-[1440px] mx-auto transition-all duration-300'>
+        {/* PERBAIKAN DI SINI:
+            - mx-auto: Memastikan container berada di tengah layar.
+            - w-full: Mengambil ruang yang tersedia.
+            - max-w-screen-xl atau 2xl: Membatasi lebar agar tidak "melar" di monitor besar.
+            - px-4: Padding agar konten tidak menempel ke pinggir di layar kecil.
+        */}
+        <div className='w-full max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8 transition-all duration-300'>
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/my-profile' element={<MyProfile />} />
             <Route path='/about' element={<About />} />
             <Route path='/services' element={<Services />} />
-              <Route path='layanan/npwpd' element={<Npwpd />} />
-              <Route path='layanan/pbjt' element={<Pbjt />} />
-              <Route path='layanan/pajakreklame' element={<PajakReklame />} />
-              <Route path='layanan/pajakairtanah' element={<PajakAirTanah />} />
-              <Route path='layanan/pbb' element={<Pbb />} />
-              <Route path='layanan/bphtb' element={<Bphtb />} />
-              <Route path='layanan/mblb' element={<PajakMBLB />} />
-              <Route path='layanan/keberatan' element={<Keberatan />} />
-              <Route path='layanan/underdevelopment' element={<UnderDevelopment />} />
+            <Route path='layanan/npwpd' element={<Npwpd />} />
+            <Route path='layanan/pbjt' element={<Pbjt />} />
+            <Route path='layanan/pajakreklame' element={<PajakReklame />} />
+            <Route path='layanan/pajakairtanah' element={<PajakAirTanah />} />
+            <Route path='layanan/pbb' element={<Pbb />} />
+            <Route path='layanan/bphtb' element={<Bphtb />} />
+            <Route path='layanan/mblb' element={<PajakMBLB />} />
+            <Route path='layanan/keberatan' element={<Keberatan />} />
+            <Route path='layanan/underdevelopment' element={<UnderDevelopment />} />
             <Route path='/news' element={<News />} />
             <Route path='/berita/:id' element={<NewsDetail />} /> 
             <Route path='/officers' element={<Officers />} />
@@ -67,7 +72,7 @@ const App = () => {
         <FloatingWA />
       </main>
 
-      {/* 3. Footer di luar container agar background-nya bisa full-width */}
+      {/* 3. Footer */}
       <Footer />
     </div>
   )
